ab997677a9b84b3436e7f54391c8b86eea5b621f
Jason Lee <lwlee2608@gmail.com>
2023-09-13 16:38:14 +0800

    Initial code for decode()

--- mdd/mdd.go
+++ mdd/mdd.go
@@ -1,9 +1,56 @@
 package mdd
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
 
-func decode() {
-	fmt.Println("Decode")
+type Header struct {
+	Key           string // position 0
+	TotalField    int    // position 1
+	Depth         int    // position 2
+	SchemaVersion int    // position 3
+	ExtVersion    int    // position 4
+}
+
+type Field struct {
+	Value string
+}
+
+type Container struct {
+	Header Header
+	Fields []Field
+}
+
+func decode(data string) Container {
+	fmt.Println("Decode ", data)
+	var container Container
+
+	// Extract header and fields substrings
+	headerEnd := strings.Index(data, ">")
+	fieldsStart := strings.Index(data, "[")
+
+	headerStr := data[1:headerEnd]
+	fieldsStr := data[fieldsStart+1 : len(data)-1]
+
+	// Decode header
+	headerParts := strings.Split(headerStr, ",")
+	if len(headerParts) == 5 {
+		container.Header.Key = headerParts[0]
+		container.Header.TotalField, _ = strconv.Atoi(headerParts[1])
+		container.Header.Depth, _ = strconv.Atoi(headerParts[2])
+		container.Header.SchemaVersion, _ = strconv.Atoi(headerParts[3])
+		container.Header.ExtVersion, _ = strconv.Atoi(headerParts[4])
+	}
+
+	// Decode fields
+	fieldsParts := strings.Split(fieldsStr, ",")[1:] // Skipping the first part as it belongs to the header
+	for _, f := range fieldsParts {
+		container.Fields = append(container.Fields, Field{Value: f})
+	}
+
+	return container
 }
 
 func encode() {
--- mdd/mdd_test.go
+++ mdd/mdd_test.go
@@ -1,8 +1,17 @@
 package mdd
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestMdd(t *testing.T) {
+	// mdc := "<1,18,0,-6,5222,2>[,(6:value2),3,2021-09-07T08:00:25.000001Z," +
+	// 	"2021-10-31,09:13:02.667997Z,88,5.5,"
+	mdc := "<1,18,0,-6,5222>[1,abc,foo,bar]"
 	encode()
-	decode()
+	container := decode(mdc)
+	fmt.Println("Decoded Container:")
+	fmt.Printf("Header: %+v\n", container.Header)
+	fmt.Printf("Fields: %+v\n", container.Fields)
 }
