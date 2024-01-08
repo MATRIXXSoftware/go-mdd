38e0dd5a26c18ebc148b8f25c7fa20d9a6a567fd
Jason Lee <lwlee2608@gmail.com>
2024-01-08 23:12:58 +0800

    add object-id struct

---
+++ mdd/field/objectid.go
@@ -0,0 +1,25 @@
+package field
+
+import (
+	"fmt"
+	"unicode"
+)
+
+type ObjectID string
+
+func NewObjectID(value string) (ObjectID, error) {
+	var colonCount, dashCount int
+	for _, c := range value {
+		if c == ':' {
+			colonCount++
+		} else if c == '-' {
+			dashCount++
+		} else if !unicode.IsDigit(c) {
+			return "", fmt.Errorf("invalid ObjectId format '%s'. Parts must be numeric", value)
+		}
+	}
+	if !(dashCount == 3 && colonCount == 0) && !(dashCount == 0 && colonCount == 3) {
+		return "", fmt.Errorf("invalid ObjectId format '%s'", value)
+	}
+	return ObjectID(value), nil
+}
