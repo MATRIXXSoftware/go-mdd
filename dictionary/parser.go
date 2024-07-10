e662643f410c767fc6ff48a80c99b17a25539811
Jason Lee <lwlee2608@gmail.com>
2024-07-10 22:06:33 +0800

    Implements xml dictionary parser

--- dictionary/parser.go
+++ dictionary/parser.go
@@ -6,8 +6,9 @@ import (
 )
 
 type Configuration struct {
-	Name     xml.Name  `xml:"configuration"`
-	Subtypes []Subtype `xml:"subtype"`
+	Name       xml.Name    `xml:"configuration"`
+	Subtypes   []Subtype   `xml:"subtype"`
+	Containers []Container `xml:"container"`
 }
 
 type Subtype struct {
@@ -21,15 +22,48 @@ type Value struct {
 	Value string `xml:",chardata"`
 }
 
-func Parse(data []byte) (*Configuration, error) {
+type Container struct {
+	ID                   string        `xml:"id,attr"`
+	DocDescription       string        `xml:"doc_description"`
+	Key                  string        `xml:"key"`
+	CreatedSchemaVersion string        `xml:"created_schema_version"`
+	DeletedSchemaVersion string        `xml:"deleted_schema_version"`
+	BaseContainer        BaseContainer `xml:"base_container,omitempty"`
+	Fields               []Field       `xml:"field"`
+}
+
+type BaseContainer struct {
+	ID   string `xml:"id,attr"`
+	Name string `xml:",chardata"`
+}
+
+type Field struct {
+	ID                   string `xml:"id,attr"`
+	DocDescription       string `xml:"doc_description"`
+	Datatype             string `xml:"datatype"`
+	IsArray              bool   `xml:"array"`
+	IsList               bool   `xml:"list"`
+	StructID             string `xml:"struct_id"`
+	SubTypeReference     string `xml:"subtype_reference"`
+	CreatedSchemaVersion string `xml:"created_schema_version"`
+	DeletedSchemaVersion string `xml:"deleted_schema_version"`
+}
+
+func Parse(reader io.Reader) (*Configuration, error) {
+	data, err := io.ReadAll(reader)
+	if err != nil {
+		return nil, err
+	}
+
 	var config Configuration
-	err := xml.Unmarshal(data, &config)
+	err = xml.Unmarshal(data, &config)
 	if err != nil {
 		return nil, err
 	}
 	return &config, nil
 }
 
-func Load(reader io.Reader) (*Dictionary, error) {
-	return nil, nil
+func Load(reader io.Reader) (*Configuration, error) {
+	// convert to Dictionary
+	return Parse(reader)
 }
